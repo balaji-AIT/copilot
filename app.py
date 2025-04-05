6c57ebe3451fd63b0888223141099d53d4e1ec83
Balaji020522 <balaji.b@ylogx.io>
2025-04-05 21:40:14 +0530

    sam

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Hello!"})
+    return jsonify({"message": "Hello sam!"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
